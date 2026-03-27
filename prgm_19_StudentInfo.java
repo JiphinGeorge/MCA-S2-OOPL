c8ccf2d0e419641d72053070c4d40750064012d9
Jiphin George <160301570+JiphinGeorge@users.noreply.github.com>
2026-03-27 10:38:25 +0530

    Implement student information storage and retrieval

--- prgm_19_StudentInfo.java
+++ prgm_19_StudentInfo.java
@@ -1,20 +1,59 @@
+/*
+Question:
+Design a system to store student information, where each student has a
+unique ID (integer) and a name (string). Use appropriate Java collection
+to efficiently store and retrieve student names based on their ID.
+*/
 
 import java.util.*;
-public class prgm_19_StudentInfo {
+
+public class Pg18_StudentManager {
 
     public static void main(String[] args) {
 
-        HashMap<Integer,String> students = new HashMap<>();
+        HashMap<Integer, String> students = new HashMap<>();
+        Scanner sc = new Scanner(System.in);
 
-        students.put(101,"Arun");
-        students.put(102,"Meera");
-        students.put(103,"Rahul");
+        int id;
+        String name;
 
-        int id = 102;
+        System.out.print("Enter number of students: ");
+        int n = sc.nextInt();
 
-        if(students.containsKey(id))
-            System.out.println("Student Name: "+students.get(id));
-        else
+        for (int i = 0; i < n; i++) {
+
+            System.out.print("Enter Student ID: ");
+            id = sc.nextInt();
+            sc.nextLine();
+
+            System.out.print("Enter Student Name: ");
+            name = sc.nextLine();
+
+            students.put(id, name);
+        }
+
+        System.out.print("Enter ID to search: ");
+        int searchId = sc.nextInt();
+
+        if (students.containsKey(searchId)) {
+            System.out.println("Student Name: " + students.get(searchId));
+        } else {
             System.out.println("Student not found");
+        }
+
+        sc.close();
     }
 }
+
+/*
+Sample Output:
+
+Enter number of students: 2
+Enter Student ID: 101
+Enter Student Name: Rahul
+Enter Student ID: 102
+Enter Student Name: Anu
+
+Enter ID to search: 101
+Student Name: Rahul
+*/
